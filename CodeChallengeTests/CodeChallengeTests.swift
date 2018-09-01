4ed33267411003b14aafbd9d575c55d68cc2ad9d
Jorge Mendoza <jorgevmendoza@gmail.com>
2018-09-01 11:17:20 -0400

    Added Services tests

--- CodeChallengeTests/CodeChallengeTests.swift
+++ CodeChallengeTests/CodeChallengeTests.swift
@@ -24,12 +24,57 @@ class CodeChallengeTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        Services.shared.retriveShowList(page: 1){
+            (show, error) in
+
+            let first = show?.first
+            XCTAssertEqual(first?.id ?? 0, 123)
+            //XCTAssertTrue(1 != 1)
+
+        }
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testShowListService(){
+        
+        Services.shared.retriveShowList(page: 1){
+            (show, error) in
+            
+            let first = show?.first
+            XCTAssertTrue((first?.id ?? 0) == 250)
+            //XCTAssertTrue(1 != 1)
+            
+        }
+        
+    }
+    
+    func testShowSearch(){
+        
+        Services.shared.searchShowByName(query: "girls"){
+            (show, error) in
+            
+            let first = show?.first?.score
+            //17.396406
+            XCTAssertEqual(first ?? 0.0, 5.0)
+            
+        }
+    }
+    
+    func testPeopleSearch(){
+        
+        Services.shared.peopleSearch(query: "lauren"){
+            (show, error) in
+            
+            let first = show?.first?.score
+            XCTAssertTrue((first ?? 0.0) == 34.579826)
+            
         }
     }
     
